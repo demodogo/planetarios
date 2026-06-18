d2137389f629b9d0f9efbf4ec8881fc245aec606
demodogo <macaigm@gmail.com>
2026-06-18 05:00:27 -0400

    smoother behavior on zoom 'cuadrante generacional'

--- src/ui/generaciones/zoom-image-hover.tsx
+++ src/ui/generaciones/zoom-image-hover.tsx
@@ -30,8 +30,8 @@ export function ZoomImageHover(props: ImageProps) {
 	return (
 		<div className="relative flex w-full flex-col items-start">
 			<div className="mb-2 flex w-full items-center justify-start gap-1.5 text-black/50 md:hidden">
-				<PinchIcon className="h-12 w-12 shrink-0" />
-				<span className="font-body-prototype whitespace-nowrap text-[0.9rem] font-bold uppercase tracking-wider">
+				<PinchIcon className="h-5 w-5 shrink-0" />
+				<span className="font-body-prototype whitespace-nowrap text-[0.7rem] font-bold uppercase tracking-wider">
 					Pinch para acercar
 				</span>
 			</div>
@@ -40,36 +40,32 @@ export function ZoomImageHover(props: ImageProps) {
 				ref={containerRef}
 				className="relative w-full cursor-auto md:cursor-none"
 				onMouseEnter={() => setIsHovering(true)}
-				onMouseLeave={() => {
-					setIsHovering(false);
-					setPosition(null);
-				}}
+				onMouseLeave={() => setIsHovering(false)}
 				onMouseMove={handleMouseMove}
 			>
 				<Image {...props} className={props.className} />
 
-				{isHovering && position && (
+				{position && (
 					<div
-						className="pointer-events-none absolute z-50 hidden overflow-hidden rounded-full border-[3px] border-white bg-white shadow-[0_10px_40px_rgb(0,0,0,0.3)] ring-1 ring-black/5 md:block"
+						className={`pointer-events-none absolute left-0 top-0 z-50 hidden overflow-hidden rounded-full border-[3px] border-white bg-white shadow-[0_10px_40px_rgb(0,0,0,0.3)] ring-1 ring-black/5 transition-opacity duration-300 ease-out md:block ${
+							isHovering ? 'opacity-100' : 'opacity-0'
+						}`}
 						style={{
 							width: `${LENS_SIZE}px`,
 							height: `${LENS_SIZE}px`,
-							left: `${position.x - LENS_SIZE / 2}px`,
-							top: `${position.y - LENS_SIZE / 2}px`,
+							transform: `translate(${position.x - LENS_SIZE / 2}px, ${position.y - LENS_SIZE / 2}px)`,
 						}}
 					>
 						<div
-							className="absolute"
+							className="absolute left-0 top-0"
 							style={{
 								width: `${position.width}px`,
 								height: `${position.height}px`,
-								left: `-${position.x - LENS_SIZE / 2}px`,
-								top: `-${position.y - LENS_SIZE / 2}px`,
-								transform: `scale(${ZOOM})`,
+								transform: `translate(-${position.x - LENS_SIZE / 2}px, -${position.y - LENS_SIZE / 2}px) scale(${ZOOM})`,
 								transformOrigin: `${(position.x / position.width) * 100}% ${(position.y / position.height) * 100}%`,
 							}}
 						>
-							<Image {...props} className={props.className} />8{' '}
+							<Image {...props} className={props.className} />
 						</div>
 					</div>
 				)}
